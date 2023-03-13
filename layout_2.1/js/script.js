0392ee3860eba1586aaeb129780763f61592e034
ansalim01 <ansalim14@gmail.com>
2023-03-13 11:16:47 +0300

    test

--- layout_2.1/js/script.js
+++ layout_2.1/js/script.js
@@ -1,17 +1,21 @@
 'use strict'
 
 
-///лайк-сердце
-let mainComment = document.querySelector('.comment')
+///---лайк-сердце---close
+const mainComment = document.querySelector('.comment')
 
 mainComment.addEventListener('click', (e) => {
-    if (!e.target.closest('.btn-like')) return
-    e.target.classList.toggle('btn-like-active')
-
+    if (e.target.closest('.btn-like')) {
+        e.target.classList.toggle('btn-like-active')
+    } else if (e.target.closest('.btn-close')) {
+        e.target.closest('.media').remove();
+    }
 })
+
+
 ////
 const formShape = document.querySelector('.shape__form');
-let shapeButton = document.querySelector('.shape__button');
+const shapeButton = document.querySelector('.shape__button');
 
 const commentName = formShape.querySelector('[name="shape__name"]');
 const commentText = formShape.querySelector('[name="shape__text"]');
@@ -35,7 +39,7 @@ commentText.addEventListener('input', () => {
 function piginationCommentName() {
     if (commentName.value.trim().length < 4) {
         commentName.classList.add('_form-error')
-        shapeErrorName.innerHTML = 'больше 4 надо'   /*---надо норм текст----------------------------*/
+        shapeErrorName.innerHTML = 'Имя должно быть больше 3 символа'
         numErrorName === 0 ? numErrorName = 1 : '';
     } else {
         commentName.classList.remove('_form-error')
@@ -46,15 +50,13 @@ function piginationCommentName() {
 function piginationCommentText() {
     if (commentText.value.trim().length <= 0) {
         commentText.classList.add('_form-error')
-        shapeErrorText.innerHTML = 'пусто както'   /*---надо норм текст----------------------------*/
+        shapeErrorText.innerHTML = 'Нельзя оставлять пустой комментарий'
         numErrorText === 0 ? numErrorText = 1 : '';
     } else {
         commentText.classList.remove('_form-error');
         shapeErrorText.innerHTML = '';
         numErrorText = 0;
     }
-
-
 }
 
 
@@ -66,13 +68,10 @@ shapeButton.addEventListener('click', (event) => {
     piginationCommentText()
     if (numErrorName === 1 || numErrorText === 1) return
 
-
     let comment = {
         name: commentName.value,
         text: commentText.value,
         date: commentDate.value,
-
-
     }
     commentName.value = '';
     commentText.value = '';
@@ -83,19 +82,20 @@ shapeButton.addEventListener('click', (event) => {
 
 
 function addComment(com) {
-
+    ///добавление 0 в числа меньше 10
     let date = new Date();
-    let today = `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}`
-        : date.getMonth() + 1}-${date.getDate() < 10 ? `0${date.getDate()}`
-            : date.getDate()}`;
 
     let time = `${date.getHours() < 10 ? `0${date.getHours()}`
         : date.getHours()}:${date.getMinutes() < 10 ? `0${date.getMinutes()}`
             : date.getMinutes()}`;
 
 
+    //время разница между 'сегодня'и 'ук.днём'
     let textDateDifference = null;
     let dateSelected = new Date(com.date);
+    if (!com.date) {
+        textDateDifference = 'Сегодня';
+    }
     if (dateSelected.getFullYear() === date.getFullYear() && dateSelected.getMonth() === date.getMonth()) {
         if (dateSelected.getDate() === date.getDate()) {
             textDateDifference = 'Сегодня';
@@ -104,16 +104,14 @@ function addComment(com) {
         }
     }
 
-
-
-
+    //добавление в html 
     let commentList = document.querySelector('.comment__list');
     commentList.insertAdjacentHTML('beforeend', `<li class="media">
     <div class="media__body">
         <div class="media__heading">
             <div class="media__name">${com.name}</div>
             <div class="media__metadate">
-                <span class="media__date">${textDateDifference ? textDateDifference : com.date ? com.date : today}, ${time}</span>
+                <span class="media__date">${textDateDifference ? textDateDifference : com.date}, ${time}</span>
                 
             </div>
         </div>
@@ -122,6 +120,7 @@ function addComment(com) {
         </div>
         <div class="media__footer">
             <div class="btn-like"></div>
+            <div class="btn-close"></div>
         </div>
     
     </div>
