5c10d0709c903cd47bb7b6993f389703d4e841c4
ansalim01 <ansalim14@gmail.com>
2023-03-15 10:12:16 +0300

    test

--- layout_2.2/js/dist/script.dev.js
+++ layout_2.2/js/dist/script.dev.js
@@ -1,23 +1,107 @@
 'use strict';
 
-function test() {
-  var f;
-  return regeneratorRuntime.async(function test$(_context) {
+var formShape = document.querySelector('.shape__form');
+var shapeButton = document.querySelector('.shape__button');
+var shapeNameRepository = document.querySelector('.shape__name-repository');
+var repositoryList = document.querySelector('.repository__list');
+document.addEventListener('keyup', function (e) {
+  if (e.code != 'Enter') return;
+  gettingData();
+});
+shapeButton.addEventListener('click', function _callee(e) {
+  return regeneratorRuntime.async(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
-          _context.next = 2;
-          return regeneratorRuntime.awrap(fetch());
+          e.preventDefault();
+          gettingData();
 
         case 2:
-          f = _context.sent;
-
-        case 3:
         case "end":
           return _context.stop();
       }
     }
   });
+});
+
+function gettingData() {
+  var inputValue, response, data;
+  return regeneratorRuntime.async(function gettingData$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          inputValue = shapeNameRepository.value;
+
+          if (!(inputValue.trim().length <= 0)) {
+            _context2.next = 4;
+            break;
+          }
+
+          repositoryList.innerHTML = '<div class="__error">Напишите что то</div>';
+          return _context2.abrupt("return");
+
+        case 4:
+          _context2.prev = 4;
+          _context2.next = 7;
+          return regeneratorRuntime.awrap(fetch("https://api.github.com/search/repositories?q=".concat(inputValue)));
+
+        case 7:
+          response = _context2.sent;
+          console.log(response);
+
+          if (!response.ok) {
+            _context2.next = 16;
+            break;
+          }
+
+          _context2.next = 12;
+          return regeneratorRuntime.awrap(response.json());
+
+        case 12:
+          data = _context2.sent;
+          addRepository(data);
+          _context2.next = 17;
+          break;
+
+        case 16:
+          alert('есть проблемы');
+
+        case 17:
+          _context2.next = 22;
+          break;
+
+        case 19:
+          _context2.prev = 19;
+          _context2.t0 = _context2["catch"](4);
+          console.log(_context2.t0);
+
+        case 22:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  }, null, null, [[4, 19]]);
 }
 
-test();
+function addRepository(data) {
+  var lengthRepository = data.items.length;
+  var itemsRepository = data.items;
+
+  if (lengthRepository === 0) {
+    repositoryList.innerHTML = '<div class="__error">Не нашёл</div>';
+    return;
+  }
+
+  repositoryList.innerHTML = '';
+
+  for (var i = 0; i < (lengthRepository > 10 ? 10 : lengthRepository); i++) {
+    var updated = createsStringUpdated(itemsRepository[i].updated_at);
+    repositoryList.insertAdjacentHTML('beforeend', "<li class=\"media\">\n        <div class=\"media__body\">\n            <div class=\"media__heading\">\n           \n                <a class=\"media__name\" href=\"".concat(itemsRepository[i].owner.html_url, "\" target=\"_blank\">\u0410\u0432\u0442\u043E\u0440: ").concat(itemsRepository[i].owner.login, " </a>\n                <a class=\"media__metadate\" href=\"").concat(itemsRepository[i].owner.html_url, "\" target=\"_blank\">\n                    <span class=\"media__date\">\u041E\u0431\u043D\u043E\u0432\u043B\u0435\u043D\u043D\u044B\u0439: ").concat(updated, "</span>\n                </a>\n            </div>\n            \n            <a class=\"media__repository\" href=\"").concat(itemsRepository[i].html_url, "\" target=\"_blank\">\n            \u0420\u0435\u043F\u043E\u0437\u0438\u0442\u043E\u0440\u0438\u0439 :\n                ").concat(itemsRepository[i].name, "\n            </a>\n        </div>\n    </li>"));
+  }
+}
+
+function createsStringUpdated(updated) {
+  var date = new Date(updated);
+  var strDate = "".concat(date.getFullYear(), "-").concat(date.getMonth() + 1 > 10 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1), "-").concat(date.getDate() > 10 ? date.getDate() : '0' + date.getDate());
+  return strDate;
+}
--- layout_2.2/js/script.js
+++ layout_2.2/js/script.js
@@ -1,8 +1,79 @@
 'use strict'
 
 
-async function test() {
-    let f = await fetch()
-}
-test()
 
+const formShape = document.querySelector('.shape__form');
+const shapeButton = document.querySelector('.shape__button');
+const shapeNameRepository = document.querySelector('.shape__name-repository');
+const repositoryList = document.querySelector('.repository__list');
+
+document.addEventListener('keyup', (e) => {
+    if (e.code != 'Enter') return
+    gettingData()
+})
+shapeButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+    gettingData();
+})
+async function gettingData() {
+    const inputValue = shapeNameRepository.value
+
+    if (inputValue.trim().length <= 0) {
+        repositoryList.innerHTML = '<div class="__error">Напишите что то</div>';
+        return
+    }
+    try {
+        let response = await fetch(`https://api.github.com/search/repositories?q=${inputValue}`);
+        console.log(response);
+        if (response.ok) {
+            const data = await response.json()
+
+            addRepository(data)
+        } else {
+            alert('есть проблемы');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+function addRepository(data) {
+    const lengthRepository = data.items.length;
+    const itemsRepository = data.items;
+
+    if (lengthRepository === 0) {
+        repositoryList.innerHTML = '<div class="__error">Не нашёл</div>';
+        return
+    }
+
+    repositoryList.innerHTML = '';
+
+    for (let i = 0; i < (lengthRepository > 10 ? 10 : lengthRepository); i++) {
+        const updated = createsStringUpdated(itemsRepository[i].updated_at)
+        repositoryList.insertAdjacentHTML('beforeend', `<li class="media">
+        <div class="media__body">
+            <div class="media__heading">
+           
+                <a class="media__name" href="${itemsRepository[i].owner.html_url}" target="_blank">Автор: ${itemsRepository[i].owner.login} </a>
+                <a class="media__metadate" href="${itemsRepository[i].owner.html_url}" target="_blank">
+                    <span class="media__date">Обновленный: ${updated}</span>
+                </a>
+            </div>
+            
+            <a class="media__repository" href="${itemsRepository[i].html_url}" target="_blank">
+            Репозиторий :
+                ${itemsRepository[i].name}
+            </a>
+        </div>
+    </li>`)
+    }
+}
+
+function createsStringUpdated(updated) {
+    const date = new Date(updated)
+    const strDate = `${date.getFullYear()}-${(date.getMonth() + 1) > 10 ? (date.getMonth() + 1)
+        : '0' + (date.getMonth() + 1)}-${date.getDate() > 10 ? date.getDate()
+            : '0' + date.getDate()}`
+    return strDate;
+}
