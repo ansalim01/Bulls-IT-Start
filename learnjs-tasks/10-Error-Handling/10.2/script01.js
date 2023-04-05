35f2cc7f488cc331322fc990f96717f09a9b5de6
ansalim01 <ansalim14@gmail.com>
2023-04-05 04:37:04 +0300

    tst

---
+++ learnjs-tasks/10-Error-Handling/10.2/script01.js
@@ -0,0 +1,16 @@
+'use strict'
+
+class FormatError extends SyntaxError {
+  constructor(message) {
+    super(message);
+    this.name = "FormatError";
+  }
+}
+
+let err = new FormatError("ошибка форматирования");
+
+alert(err.message);
+alert(err.name);
+alert(err.stack);
+
+alert(err instanceof SyntaxError); 
